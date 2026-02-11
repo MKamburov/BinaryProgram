11f400a36c3c28affefe4c77c546d2029c0927d6
stefancho <stefanstoqnov@gmail.com>
2026-02-11 10:28:21 +0200

    Add project files.

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding =
+                Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Въведете десетично число");
+            int n = int.Parse(Console.ReadLine());
+            string binary = "";
+            while (n > 0)
+            {
+                binary = (n % 2) + binary;
+                n /= 2;
+
+
+            }
+            Console.WriteLine(binary);
+        }
+    }
+}
